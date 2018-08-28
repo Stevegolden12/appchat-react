0673b82eda3ad0ac31cd25b00f84510912165b4e
Steve Golden <sgolden2525@outlook.com>
2018-08-27 19:40:08 -0600

    Starting appchat for react
    Created files for an chat app for react

---
+++ config.js
@@ -0,0 +1,5 @@
+const tokenUrl =
+  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/69f3847f-cbb6-4623-8855-84d4dee5cc63/token";
+const instanceLocator = "v1:us1:69f3847f-cbb6-4623-8855-84d4dee5cc63";
+
+export { tokenUrl, instanceLocator }
